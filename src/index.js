82acb6d85a92519c48ffe39d1e46548777856cb3
Sidney <sid.14boys@yahoo.fr>
2025-05-11 09:57:24 -0700

    Add @vercel/analytics dependency and integrate Analytics component into the BettingDashboard for enhanced user tracking and insights.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import BettingDashboard from "./BettingDashboard";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BettingDashboard />
+    <Analytics />
   </React.StrictMode>
 );
