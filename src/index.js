54ad6bee9849b29b87e0d8b6dd7ec59ac7350c22
Sidney <sid.14boys@yahoo.fr>
2025-05-11 10:36:14 -0700

    Add test deploy comment in index.js for deployment verification

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import BettingDashboard from "./BettingDashboard";
 import { Analytics } from "@vercel/analytics/react";
 
+// test deploy
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
